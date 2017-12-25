43914003502dd6cd59ee2ea8349ad75ef7a9c35d
Zack <zak5325@gmail.com>
2017-12-25 23:49:00 +0800

    modify something

--- face_detect.py
+++ face_detect.py
@@ -1,25 +1,35 @@
 import numpy as np
 import cv2
 import os
-test='image'
-os.makedirs(test)
-os.chdir(os.getcwd()+'/'+test)
-c=1
+import shutil
+def create_path(name):#remove and create path
+	if not os.path.isdir(name):
+		os.makedirs(name)
+	else:
+		shutil.rmtree(name)
+		os.makedirs(name)
+#change the dir name below:
+filename='image'
+create_path(filename)
+os.chdir(os.getcwd()+'/'+filename)
+
 face_cascade=cv2.CascadeClassifier('/Users/lirt/workspace/opencv_test/haarcascades/haarcascade_frontalface_default.xml')
+#import cascade model(?)
 cap = cv2.VideoCapture(0)
+#open camera
+count=1
 while(cap.isOpened()):
 	ret, frame = cap.read()
-
 	#frame stands for you get the next frame from cap.read function with the camera
-	#ret stands for true or flase, for checking whether the frame can get value or not 
+	#ret stands for true or false, for checking whether the frame can get value or not 
 	faces=face_cascade.detectMultiScale(frame,1.3,5)
 	#detect the picture with face_cascade model with 1.3 stands for scaleFactor and 5 stands for minNeighbors
 	#scalefactor:Detection of particle size.The larger the value,the faster the detection speed, but the less accurate.
 	#minNeighbors:The detected number of detected frames
 	for (x,y,w,h) in faces:
 		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-		cv2.imwrite(str(c)+'.jpg',frame)
-		c=c+1
+		cv2.imwrite(str(count)+'.jpg',frame)
+		count=count+1
 		#deploy a rectangle with (x,y) to (x+w,y+h) size's,and color(255,0,0),2 stands for thickness of that rectangle
 	cv2.imshow('frame',frame)
 	#show what camera get and the rectangle you put etc.
